1ea047017b224b97641ee553cdeb1d59d5b5f750
Ryan Susana <ryansusana@live.com>
2018-12-23 19:24:01 +0100

    Initial setup

--- src/main/java/com/elepy/examples/basic/Main.java
+++ src/main/java/com/elepy/examples/basic/Main.java
@@ -1,4 +1,35 @@
 package com.elepy.examples.basic;
 
+import com.elepy.Elepy;
+import com.elepy.admin.ElepyAdminPanel;
+import com.github.fakemongo.Fongo;
+import com.mongodb.DB;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+
 public class Main {
+    public static void main(String[] args) {
+        Logger.getRootLogger().setLevel(Level.INFO);
+        org.apache.log4j.BasicConfigurator.configure();
+        //Embedded MongoDB instance
+        Fongo fongo = new Fongo("examples");
+        DB exampleDB = fongo.getDB("example1");
+
+        new Elepy()
+                //Attach a singleton to Elepy. To use Mongo with elepy, you need to attach a DB
+                .attachSingleton(DB.class, exampleDB)
+
+                //The port Elepy will run on.
+                .onPort(7777)
+
+                //Add your first Elepy Annotated Model. You can alternatively add an entire package of models.
+                .addModel(Product.class)
+
+                //Add an Elepy extension. The AdminPanel is a great start :D
+                .addExtension(new ElepyAdminPanel())
+
+                //Start Elepy!
+                .start();
+    }
 }
---
+++ src/main/java/com/elepy/examples/basic/Product.java
@@ -0,0 +1,112 @@
+package com.elepy.examples.basic;
+
+import com.elepy.annotations.Number;
+import com.elepy.annotations.*;
+import com.elepy.models.TextType;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+
+
+@RestModel(name = "Products", slug = "/products")//The only necessary annotation for Elepy
+//@JsonIgnoreProperties(ignoreUnknown = true)
+public class Product {
+
+    @Identifier // All elepy models must have atleast 1 identifying field. By default it can be a 'String productId;'
+    @PrettyName("Product ID") // A nice name to be used in Elepy error messages and such
+    @JsonProperty("productId")
+    private String productId;
+
+
+    @PrettyName("Short Description")
+    @Text(value = TextType.TEXTAREA, maximumLength = 100) //Textarea with a maximum of 100 characters
+    private String shortDescription;
+
+    @PrettyName("Long Description")
+    @Text(TextType.HTML)//WYSIWYG editor
+    private String htmlDescription;
+
+    @PrettyName("Product Name")
+    @Required // All products must have a name
+    @Uneditable // You can't edit the product's name after it has been set
+    @Unique // Product  names must be unique
+    private String name;
+
+    @PrettyName("Product Price")
+    @Number(minimum = 0)
+    private BigDecimal price;
+
+    @PrettyName("Amount of stock left")
+    @Number(minimum = 0)
+    private int stockLeft;
+
+    @PrettyName("Amount sold")
+    @Number(minimum = 0)
+    private int amountSold;
+
+    @Generated
+    @JsonProperty("revenue")
+    //Automatically generate the revenue of a product and display it in Elepy
+    public BigDecimal getRevenue(){
+        return price.multiply(BigDecimal.valueOf(amountSold));
+    }
+
+    //Getters and Setters. I like to use Lombok to automate this.
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getHtmlDescription() {
+        return htmlDescription;
+    }
+
+    public void setHtmlDescription(String htmlDescription) {
+        this.htmlDescription = htmlDescription;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getStockLeft() {
+        return stockLeft;
+    }
+
+    public void setStockLeft(int stockLeft) {
+        this.stockLeft = stockLeft;
+    }
+
+    public int getAmountSold() {
+        return amountSold;
+    }
+
+    public void setAmountSold(int amountSold) {
+        this.amountSold = amountSold;
+    }
+}
