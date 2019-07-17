c2db0a63d78681e63c84acd521351f88458e7d4d
Ryan Susana <ryansusana@live.com>
2019-07-17 12:37:48 +0200

    Revamped versions to  BETA 16

--- src/main/java/com/elepy/examples/basic/Main.java
+++ src/main/java/com/elepy/examples/basic/Main.java
@@ -5,22 +5,22 @@ import com.elepy.admin.AdminPanel;
 import com.elepy.mongo.MongoConfiguration;
 
 public class Main {
-    public static void main(String[] args) {
-        new Elepy()
-                // Choose the port
-                .onPort(8080)
+public static void main(String[] args) {
+    new Elepy()
+            // Choose the port
+            .onPort(8080)
 
-                // Add the AdminPanel configuration
-                .addConfiguration(AdminPanel.newAdminPanel())
+            // Add the AdminPanel configuration
+            .addConfiguration(AdminPanel.newAdminPanel())
 
-                // Add the Mongo configuration
-                .addConfiguration(MongoConfiguration.inMemory())
+            // Add the Mongo configuration
+            .addConfiguration(MongoConfiguration.inMemory())
 
-                // Add your model
-                .addModel(Product.class)
+            // Add your model
+            .addModel(Product.class)
 
-                // Begin your journey at http://localhost:8080/admin :D!
-                .start();
+            // Begin your journey at http://localhost:8080/admin :D!
+            .start();
 
-    }
+}
 }
